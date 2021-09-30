bd77efc8df872c01390b6e17f21f21c65b1c0cfa
MashaalSayeed <mashaal2403@gmail.com>
2021-09-30 19:04:24 +0400

    Adding cytoscape

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,36 @@
 <template>
   <h1>Interactive Data Visualisation</h1>
-  <div id="list-rendering">
-  <ol>
-    <li v-bind:key="node.identity" v-for="node in nodes">
-      {{ node.labels[0] }} {{ node.properties.name }} {{ node.properties.code }}
-    </li>
-  </ol>
+  <div id="cy">
   </div>
 </template>
 
 <script lang="ts">
 import { Vue } from 'vue-class-component';
 import axios from 'axios';
+import cytoscape from 'cytoscape';
 
 export default class App extends Vue {
-  nodes?: any[];
-
-  data() {
-    return {
-      nodes: []
-    }
-  }
+  cytoscape?: cytoscape.Core;
 
   mounted(): void {
+    // Initialise cytoscape
+    this.cytoscape = cytoscape({
+      container: document.getElementById('cy')
+    })
+
+    // Test connection
     axios.get('http://readmythoughts.ddns.net:7070/', {
       params: {
         string : 'MATCH (n:Dataset) WHERE id(n) = $id RETURN n',
         data : { id : 2131 }
       }
     }).then((response: any) =>{
-        console.log("RESPONSE BODY");
-        this.nodes = response.data
+        const nodes = response.data.map((node: any) => node._fields[0]);
+        console.log(nodes)
+        this.cytoscape!.add({
+          group: 'nodes',
+          data: nodes[0].identity.low
+        })
     })
   }
 }
