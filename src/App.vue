c4320663c99e64f32d1188a23b48f649ff198cb3
MashaalSayeed <mashaal2403@gmail.com>
2021-10-01 09:14:04 +0400

    api requests

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <h1>Interactive Data Visualisation</h1>
+  <input type="text" v-model="nodeID" placeholder="Search ID">
+  <button v-on:click="fetchNodeInfo">Search</button>
   <div id="cy">
   </div>
 </template>
@@ -9,29 +11,80 @@ import { Vue } from 'vue-class-component';
 import axios from 'axios';
 import cytoscape from 'cytoscape';
 
+interface NodeProperties {
+  description: string;
+  identifier: string;
+  issued: string;
+  landingPage: string;
+  name: string;
+}
+
+interface Node {
+  identity: number;
+  type: string;
+  properties: NodeProperties;
+}
+
+
 export default class App extends Vue {
   cytoscape?: cytoscape.Core;
+  nodeID!: number;
 
-  mounted(): void {
+  data(): any {
+    return {
+      nodeID: 0
+    }
+  }
+
+  async mounted(): Promise<void> {
+    console.log('mounted')
     // Initialise cytoscape
     this.cytoscape = cytoscape({
-      container: document.getElementById('cy')
+      container: document.getElementById('cy'),
+      layout: { name: 'grid', rows: 1 }
+    })
+  }
+
+  async fetchNodeInfo(): Promise<Node> {
+    const response = await axios.get('/Dataset/info', { params: {identity: this.nodeID}})
+    const node = response.data[0] as Node
+    console.log(node)
+
+    this.cytoscape!.add({
+      group: 'nodes',
+      data: { 
+        id: 'n' + node.identity.toString(),
+        ...node.properties
+      },
+      position: { x: 100, y: 100 }
     })
 
-    // Test connection
-    axios.get('http://readmythoughts.ddns.net:7070/', {
-      params: {
-        string : 'MATCH (n:Dataset) WHERE id(n) = $id RETURN n',
-        data : { id : 2131 }
-      }
-    }).then((response: any) =>{
-        const nodes = response.data.map((node: any) => node._fields[0]);
-        console.log(nodes)
-        this.cytoscape!.add({
-          group: 'nodes',
-          data: nodes[0].identity.low
-        })
-    })
+    await this.getAllRelations()
+    return node
+  }
+  
+  async getAllRelations(): Promise<void> {
+    const response = await axios.get('/Node/getAllRelations', { params: {identity: this.nodeID} })
+    console.log(response.data[0])
+    console.log(response.data.length)
+
+    for (let data of response.data) {
+      this.cytoscape!.add([{
+        group: 'nodes',
+        data: { 
+          id: 'n' + data.node.identity.toString(), 
+          ...data.node
+        }
+      }, {
+        group: 'edges',
+        data: { 
+          id: 'e' + data.relation.identity.toString(), 
+          type: data.relation.type,
+          source: 'n' + this.nodeID.toString(),
+          target: 'n' + data.node.identity
+        },
+      }])
+    }
   }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://readmythoughts.ddns.net:7070/'
 
 createApp(App).mount('#app')
