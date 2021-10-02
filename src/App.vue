cad4d5d3481aee9160ca94412e48092946e5b5b9
MashaalSayeed <mashaal2403@gmail.com>
2021-10-02 18:03:59 +0400

    node traversing Co-authored-by: Christo Joby Antony

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="nav-bar-top">Navigation Bar</div>
-  <div class="nav-bar-left">
-        <h5 id="">
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">
+        <img src="./assets/icon.png">
+        The Black Sheep
+      </a>
+    </div>
+  </nav>
+  <div class="row main">
+
+    <div class="col s3 nav-bar-left card">
+      <div class="card-content">
+        <span class="card-title">
             {{ node === null ? "Select A Node to Know more about" : node.properties.name }}
-        </h5>
-        <h6 id="node-type"></h6>
-        <ol id="node-properties" v-if="node !== null">
-            <!-- Fill in with node info -->
-            <div>Description: {{ node.properties.description }}</div>
-            <div>Landing Page: {{ node.properties.landingPage }}</div>
-            <button>View Relations</button>
-        </ol>
+        </span>
+        <h6 id="node-type" v-if="node !== null">
+          Type: {{ node.type }}
+        </h6>
+        <div id="node-properties" v-if="node !== null">
+          <!-- Fill in with node info -->
+          <button class="waves-effect waves-light btn" style="width: 100%" @click="setNode">View Relations</button>
+          
+          <ul v-bind:key="name" v-for="(value, name) in node.properties" class="collection">
+             <li class="collection-item" v-if="name !== 'name'">{{ name }}: {{ value }} </li>
+          </ul>
+         
+        </div>
+      </div>
     </div>
 
-  <div id="cy" class="content"></div>
+    <div class="col s10"> </div>
+    <div id="cy" class="content"></div>
+  </div>
+
+  
 </template>
 
 <script lang="ts">
@@ -43,37 +63,43 @@ export default class App extends Vue {
 
   data(): any {
     return {
-      nodeID: 2131,
       node: null
     };
   }
 
   async mounted(): Promise<void> {
     console.log("mounted");
-    await this.createPrimaryNode(this.nodeID)
-    this.cytoscape!.on('tap', 'node', this.updateInfoPanel)
+    await this.setUpCytoscape(2131)
+    await this.addRelations(2131)
+
+    this.cytoscape!.on('select', 'node', async (evt) => {
+      this.node = await this.fetchNode(evt.target.id())
+    })
+
+    this.cytoscape!.on('taphold', 'node', async (evt) => {
+      this.node = await this.fetchNode(evt.target.id())
+      await this.setNode()
+    })
   }
 
-  async fetchDataset(nodeID: number): Promise<Dataset> {
-    const res = await axios.get("/Dataset/info", {
+  async fetchNode(nodeID: number): Promise<Dataset> {
+    const res = await axios.get("/Node/info", {
       params: { identity: nodeID },
     });
     return res.data;
   }
 
-  async fetchDatasetRelations(nodeID: number): Promise<any> {
+  async fetchNodeRelations(nodeID: number): Promise<any> {
     const res = await axios.get("/Node/getAllRelations", {
       params: { identity: nodeID },
     });
     return res.data;
   }
-
-  async createPrimaryNode(nodeID: number): Promise<void> {
-    const dataset = await this.fetchDataset(nodeID);
-    const relationships = await this.fetchDatasetRelations(nodeID);
-    this.node = dataset;
-    console.log(relationships);
-
+  
+  async setUpCytoscape(id: number): Promise<void> {
+    console.log('Setting up cytoscape');
+    const dataset = await this.fetchNode(id)
+    this.node = dataset
     let nodes: cytoscape.ElementDefinition[] = [{
       group: "nodes",
       data: { id : dataset.identity.toString(), name: dataset.properties.name },
@@ -81,45 +107,41 @@ export default class App extends Vue {
       selected: true
     }]
 
-    for (const data of relationships) {
-      nodes.push({
-        group: 'nodes',
-        data: { id: data.node.identity, name: data.node.name },
-        classes: data.node.type
-      })
-
-      nodes.push({
-        group: 'edges', 
-        data: { 
-          id: data.relation.identity, 
-          name: data.node.type, 
-          source: data.node.identity, 
-          target: dataset.identity 
-        }, 
-        classes: data.relation.type
-      })
-    }
-
     const config = Object.assign(cytoscapeConfig, {
       container: document.getElementById('cy'),
       elements: nodes
     })
 
-    console.log(dataset)
     this.cytoscape = cytoscape(config as cytoscape.CytoscapeOptions)
   }
 
-  async updateInfoPanel(evt: cytoscape.EventObject) : Promise<void> {
-      const node = evt.target
-      console.log('Tapped ' + node.id())
-      const response = await axios.get('/Node/info', {params: { identity : node.id()}})
-      this.node = response.data
-      console.log(response.data)
+  async addRelations(id: number) {
+      const relationships = await this.fetchNodeRelations(id)
+      let nodes: cytoscape.ElementDefinition[] = []
+      // Add the relationships and their corresponding nodes
+      for (const data of relationships) {
+        nodes.push({
+          group: 'nodes',
+          data: { id: data.node.identity, name: data.node.name },
+          classes: data.node.type
+        })
+        nodes.push({
+          group: 'edges', 
+          data: {id: data.relation.identity, name: data.node.type, source: id, target: data.node.identity },
+          classes: data.relation.type
+        })
+      }
+
+      this.cytoscape!.add(nodes)
+      this.cytoscape!.layout(cytoscapeConfig.layout).run()
+    }
+
+  async setNode(): Promise<void> {
+    console.log('Primary Node Update : ' + this.node?.identity);
+    this.cytoscape!.remove(`[id != "${this.node?.identity}"]`)
+    await this.addRelations(this.node!.identity)
   }
-  
-  async getRelations(nodeID : number) : Promise<void> {
-    const response = 'test'   
-  }
+
 }
 </script>
 
