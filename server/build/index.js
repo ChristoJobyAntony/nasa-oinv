138a397ea1d583a394e2a6a62962f76c043e2f06
Christo Joby Antony <christotantony2003@gmail.com>
2021-10-02 23:27:37 +0400

    removed build

--- server/build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-server_1.app.listen(7070, '0.0.0.0', function () {
-    console.log('Server Running here 👉 https://readmythoughts.ddns.net:7070');
-});
-//# sourceMappingURL=index.js.map
