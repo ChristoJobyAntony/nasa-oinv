0c912b24cf70bb624b410947e1769648a2e8214d
MashaalSayeed <mashaal2403@gmail.com>
2021-10-02 12:01:14 +0400

    cytoscape done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <h1>Interactive Data Visualisation</h1>
-  <input type="text" v-model="nodeID" placeholder="Search ID">
-  <button v-on:click="fetchNodeInfo">Search</button>
-  <div id="cy">
-  </div>
+  <div class="nav-bar-top">Navigation Bar</div>
+  <div class="nav-bar-left">Nav-Bar Left</div>
+
+  <div id="cy" class="content"></div>
 </template>
 
 <script lang="ts">
-import { Vue } from 'vue-class-component';
-import axios from 'axios';
-import cytoscape from 'cytoscape';
+import { Vue } from "vue-class-component";
+import axios from "axios";
+import cytoscape from "cytoscape";
+import cytoscapeConfig from '@/assets/json/cytoscapeConfig.json'
 
-interface NodeProperties {
+interface DatasetProperties {
   description: string;
   identifier: string;
   issued: string;
@@ -19,12 +19,11 @@ interface NodeProperties {
   name: string;
 }
 
-interface Node {
+interface Dataset {
   identity: number;
   type: string;
-  properties: NodeProperties;
+  properties: DatasetProperties;
 }
-
 
 export default class App extends Vue {
   cytoscape?: cytoscape.Core;
@@ -32,70 +31,71 @@ export default class App extends Vue {
 
   data(): any {
     return {
-      nodeID: 0
-    }
+      nodeID: 2131,
+    };
   }
 
   async mounted(): Promise<void> {
-    console.log('mounted')
+    console.log("mounted");
     // Initialise cytoscape
-    this.cytoscape = cytoscape({
-      container: document.getElementById('cy'),
-      layout: { name: 'grid', rows: 1 }
-    })
+    await this.createPrimaryNode(this.nodeID)
   }
 
-  async fetchNodeInfo(): Promise<Node> {
-    const response = await axios.get('/Dataset/info', { params: {identity: this.nodeID}})
-    const node = response.data[0] as Node
-    console.log(node)
-
-    this.cytoscape!.add({
-      group: 'nodes',
-      data: { 
-        id: 'n' + node.identity.toString(),
-        ...node.properties
-      },
-      position: { x: 100, y: 100 }
-    })
-
-    await this.getAllRelations()
-    return node
+  async fetchDataset(nodeID: number): Promise<Dataset> {
+    const res = await axios.get("/Dataset/info", {
+      params: { identity: nodeID },
+    });
+    return res.data;
   }
-  
-  async getAllRelations(): Promise<void> {
-    const response = await axios.get('/Node/getAllRelations', { params: {identity: this.nodeID} })
-    console.log(response.data[0])
-    console.log(response.data.length)
 
-    for (let data of response.data) {
-      this.cytoscape!.add([{
+  async fetchDatasetRelations(nodeID: number): Promise<any> {
+    const res = await axios.get("/Node/getAllRelations", {
+      params: { identity: nodeID },
+    });
+    return res.data;
+  }
+
+  async createPrimaryNode(nodeID: number): Promise<void> {
+    const dataset = await this.fetchDataset(nodeID);
+    const relationships = await this.fetchDatasetRelations(nodeID);
+    console.log(dataset);
+    console.log(relationships);
+
+    let nodes: cytoscape.ElementDefinition[] = [{
+      group: "nodes",
+      data: { id : dataset.identity.toString(), name: dataset.properties.name },
+      classes: 'Dataset', 
+      selected: true
+    }]
+
+    for (const data of relationships) {
+      nodes.push({
         group: 'nodes',
+        data: { id: data.node.identity, name: data.node.name },
+        classes: data.node.type
+      })
+
+      nodes.push({
+        group: 'edges', 
         data: { 
-          id: 'n' + data.node.identity.toString(), 
-          ...data.node
-        }
-      }, {
-        group: 'edges',
-        data: { 
-          id: 'e' + data.relation.identity.toString(), 
-          type: data.relation.type,
-          source: 'n' + this.nodeID.toString(),
-          target: 'n' + data.node.identity
-        },
-      }])
+          id: data.relation.identity, 
+          name: data.node.type, 
+          source: data.node.identity, 
+          target: dataset.identity 
+        }, 
+        classes: data.relation.type
+      })
     }
+
+    console.log(nodes);
+    const config = Object.assign(cytoscapeConfig, {
+      container: document.getElementById('cy'),
+      elements: nodes
+    })
+    console.log(config);
+    this.cytoscape = cytoscape(config as cytoscape.CytoscapeOptions)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="@/assets/styles/main.css"></style>
